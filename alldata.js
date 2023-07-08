eec8f832e4755b1294d196cf2ed255f053b2b167
Airiel Altemara <vsokord#gmail.com>
2023-07-08 16:36:14 -0700

    upated changes to data & index.js for current user

--- alldata.js
+++ alldata.js
@@ -1,7 +1,10 @@
 function AllData() {
     const ctx = React.useContext(UserContext);
     return (
-        <h1>AllData<br/>
-        {JSON.stringify(ctx)}</h1>
+        <>
+            <h1>AllData<br />
+                {JSON.stringify(ctx)}</h1>
+            <h1>{JSON.stringify(ctx.users[0].balance)}</h1>
+        </>
     )
 }
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function Spa() {
     return (
         <HashRouter>
-            <NavBar/>
-            <UserContext.Provider value={{users:[{name:'able', email:'able@mit.edu', password:'secret', balance:100}]}}>
-            <Route path ="/" exact component={Home} />
-            <Route path ="/CreateAccount/" exact component={CreateAccount} />
-            <Route path ="/login/" exact component={Login} />
-            <Route path ="/deposit/" exact component={Deposit} />
-            <Route path ="/withdraw/" exact component={Withdraw} />
-            <Route path ="/balance/" exact component={Balance} />
-            <Route path ="/alldata/" exact component={AllData} />
+            <NavBar />
+            <UserContext.Provider value={{ currentUser: null, users: [{ name: 'able', email: 'able@mit.edu', password: 'secret', balance: 100 }] }}>
+                <Route path="/" exact component={Home} />
+                <Route path="/CreateAccount/" exact component={CreateAccount} />
+                <Route path="/login/" exact component={Login} />
+                <Route path="/deposit/" exact component={Deposit} />
+                <Route path="/withdraw/" exact component={Withdraw} />
+                <Route path="/balance/" exact component={Balance} />
+                <Route path="/alldata/" exact component={AllData} />
             </UserContext.Provider>
         </HashRouter>
-        
+
     )
 }
 
 ReactDOM.render(
-    <Spa/>,
+    <Spa />,
     document.getElementById('root')
-  );
+);
 
