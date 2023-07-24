3ab447c7203082d948349706078889079244a279
Airiel Altemara <vsokord#gmail.com>
2023-07-24 12:37:58 -0700

    login button test

--- bad-bank/src/App.test.js
+++ bad-bank/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('checks click on login button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole('button');
+  expect(buttonElement).toBeInTheDocument();
+  fireEvent.click(buttonElement)
 });
+
