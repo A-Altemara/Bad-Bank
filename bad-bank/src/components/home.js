73a403c06b642752bcdae5136202d30db07409fe
Airiel Altemara <vsokord#gmail.com>
2023-07-28 11:03:15 -0700

    updated css to match correct app

--- bad-bank/src/components/home.js
+++ bad-bank/src/components/home.js
@@ -3,15 +3,19 @@ import imageLarge from '../../src/pictures/marble-bank.png'
 
 
 export function Home() {
+    const cardStyle = {
+        textAlign: 'center',
+        justifyContent: 'center',
+    };
 
     return (
         <>
-            <Card
+            <Card style={cardStyle}
                 bgcolor="primary"
-                txtcolor="black"
-                header="BadBank Landing Page"
-                title="Welcome to the bank."
-                text="You can use this bank."
+                txtcolor="white"
+                header="BadBank™"
+                title="Welcome to your trusted banking intitiution"
+                text="Enjoy 24/7 digital banking and we cover the opening $100 deposit!"
                 body={<img src={imageLarge} className="img-fluid" alt="marble bank" />}
             />
 
