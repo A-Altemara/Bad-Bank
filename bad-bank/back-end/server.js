5751f5ea3aa72ba2c2726ab9481516c0acceaaee
Airiel Altemara <107080531+A-Altemara@users.noreply.github.com>
2023-11-06 16:36:25 -0800

    changed server for routes

--- bad-bank/back-end/server.js
+++ bad-bank/back-end/server.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
