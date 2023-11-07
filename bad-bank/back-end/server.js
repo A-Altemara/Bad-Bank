3b99bada7f19255e8770a6b66fc9796a59fcede5
Airiel Altemara <107080531+A-Altemara@users.noreply.github.com>
2023-11-06 18:10:31 -0800

    fix static

--- bad-bank/back-end/server.js
+++ bad-bank/back-end/server.js
@@ -44,10 +44,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 // Serve Swagger documentation
